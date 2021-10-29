b2beeae36505561cfa27774f198068ea8e9221c8
AnthonyZet <67970914+AnthonyZet@users.noreply.github.com>
2021-10-29 20:48:03 +0200

    complete?

--- routes/articles.js
+++ routes/articles.js
@@ -6,25 +6,50 @@ router.get('/new', (req, res) => {
     res.render('articles/new', { article: new Article() })
   })
 
+  router.get('/edit/:id', async (req, res) => {
+      const article = await Article.findById(req.params.id)
+    res.render('articles/edit', { article: article })
+  })
+ 
+ 
+
 router.get('/:slug', async (req, res) => {
     let article = await Article.findOne({ slug: req.params.slug})
     if(article == null) res.redirect('/') 
     res.render('articles/show', { article: article})
 })
 
-router.post('/', async (req, res) => {
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown
-    })
-    try{
-       article =  await article.save()
-       res.redirect(`/articles/${article.slug}`)
-    } catch (e) {
-        console.log(e)
-        res.render('articles/new', { article: article})
-    }
-    
-})
+router.post('/', async (req, res, next) => {
+    req.article = newArticle()
+    next()
+}, saveArticleAndRedirect('new'))
+
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, saveArticleAndRedirect('edit'))
+
+
+router.delete('/:id', async (req, res) => {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+  })
+  
+  function saveArticleAndRedirect(path) {
+      return async (req, res) => {
+          let article = req.article
+            article.title = req.body.title,
+            article.description = req.body.description,
+            article.markdown = req.body.markdown
+        
+        try{
+           article =  await article.save()
+           res.redirect(`/articles/${article.slug}`)
+        } catch (e) {
+           
+            res.render(`articles/${path}`, { article: article})
+        }  
+      }
+  }
+
 module.exports = router
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 mongoose.connect('mongodb://localhost/blog', {useNewUrlParser: true, useUnifiedTopology: true})
@@ -11,6 +12,7 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({
     extended: false
 }))
+app.use(methodOverride('_method'))
 
 
 app.get('/', async (req, res) => {
